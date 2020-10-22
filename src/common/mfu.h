f5a7662fcb2fc16127dcc654ebe62b0501b27ef2
Mohamad Chaarawi <mohamad.chaarawi@intel.com>
2020-10-22 00:13:57 +0000

    integration with pfind app
    Signed-off-by: Mohamad Chaarawi <mohamad.chaarawi@intel.com>

--- src/common/mfu.h
+++ src/common/mfu.h
@@ -28,6 +28,54 @@ extern "C" {
 #include "mfu_flist.h"
 #include "mfu_pred.h"
 #include "mfu_bz2.h"
+#include <regex.h>
+
+typedef struct {
+  // https://www.gnu.org/software/findutils/manual/html_mono/find.html
+  char * workdir;
+  int just_count;
+  int print_by_process;
+  char * results_dir;
+  int stonewall_timer;
+  int print_rates;
+
+  char * timestamp_file;
+  char * name_pattern;
+  regex_t name_regex;
+  uint64_t size;
+  // optimizing parameters
+  int queue_length;
+  int max_entries_per_iter;
+  int steal_from_next; // if true, then steal from the next process
+  int parallel_single_dir_access; // if 1, use hashing to parallelize single directory access, if 2 sequential increment
+
+  int stonewall;
+  int verbosity;
+} pfind_options_t;
+
+typedef struct {
+  uint64_t job_steal_inbound;
+  uint64_t work_send;
+  uint64_t job_steal_tries;
+  uint64_t work_stolen;
+  uint64_t job_steal_mpitime_us; // microseconds spend in job steal attempts (MPI)
+  uint64_t completion_tokens_send;
+} pfind_monitoring_t;
+
+typedef struct{
+  uint64_t found_files;
+  uint64_t total_files;
+  double runtime;
+  double rate;
+  uint64_t errors;
+
+  uint64_t unknown_file;
+  uint64_t checked_dirents;
+  pfind_monitoring_t monitor;
+  MPI_Comm com;
+} pfind_find_results_t;
+
+pfind_find_results_t * pfind_find(pfind_options_t * opt);
 
 #endif /* MFU_H */
 
